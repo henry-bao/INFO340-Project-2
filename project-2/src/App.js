a71a3be98923587b2090fcb406f550a840cc1373
LeoFanguw <rfang2@uw.edu>
2021-03-04 22:55:31 +0800

    basic setups

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import groupTool from 'lodash';
+import {useState} from 'react';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>HelloWord</h1>
   );
 }
 
